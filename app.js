84d3a6b9ecfb8fbc716807afcd520eb800466db5
jose <josewkik@gmail.com>
2022-12-05 09:22:48 -0300

    index unindo header e footer

--- app.js
+++ app.js
@@ -8,21 +8,11 @@ app.get('/', (req, res)=> { // quando acessar o locahost:3000/
   res.sendFile(__dirname + "/views/index.html") //buscando arquivo index.html na pasta views
 }) 
 
-app.get('/header', (req, res)=> { // quando acessar o locahost:3000/header
-    res.sendFile(__dirname + "/views/header.html") //buscando arquivo header.html na pasta views
-}) 
-
-app.get('/footer', (req, res)=> { // quando acessar o locahost:3000/footer
-   res.sendFile(__dirname + "/views/footer.html") //buscando arquivo footer.html na pasta views
-})
 
 app.get('/paginaDeProduto', (req, res)=> { // quando acessar o locahost:3000/paginaDeProduto
   res.sendFile(__dirname + "/views/paginaDeProduto.html") //buscando arquivo paginaDeProduto.html na pasta views
 })
 
-app.get('/home', (req, res)=> { // quando acessar o locahost:3000/paginaDeProduto
-  res.sendFile(__dirname + "/views/home.html") //buscando arquivo paginaDeProduto.html na pasta views
-})
 
 app.get('/listagemdeprodutos', (req, res)=> { // quando acessar o locahost:3000/paginaDeProdutos
   res.sendFile(__dirname + "/views/listagemDeProdutos.html") //buscando arquivo paginaDeProdutos.html na pasta views
@@ -50,6 +40,9 @@ app.get('/finalizacaodecompra', (req, res)=> { // quando acessar o locahost:3000
 
 app.get('/painelusuario', (req, res)=> { // quando acessar o locahost:3000/paginaDeProduto
   res.sendFile(__dirname + "/views/painelUsuario.html") //buscando arquivo paginaDeProduto.html na pasta views
+})
+app.get('/home',(req,res)=>{
+  res.sendFile(__dirname + "/views/home.html")
 })
 
 app.listen(3000,()=>{
